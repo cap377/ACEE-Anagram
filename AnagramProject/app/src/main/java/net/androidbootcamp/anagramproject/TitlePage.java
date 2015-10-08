67a4e70753d7591fbe07cfa4b7625f8396d356a6
Erik <eld66@nau.edu>
2015-10-07 22:38:33 -0700

    Title Page with links to 'AboutPage' and 'StartPage'
    I created the Title Page with all the buttons.

---
+++ AnagramProject/app/src/main/java/net/androidbootcamp/anagramproject/TitlePage.java
@@ -0,0 +1,67 @@
+package net.androidbootcamp.anagramproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+//http://android-er.blogspot.com/2012/02/apply-animation-on-button.html
+
+public class TitlePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_page);
+
+        Button button1 = (Button) findViewById(R.id.startbutton);
+        Button button2 = (Button) findViewById(R.id.aboutbutton);
+
+        final Animation anime = AnimationUtils.loadAnimation(this, R.anim.anim_scale_change);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(anime);
+                startActivity(new Intent(TitlePage.this, StartPage.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(anime);
+                startActivity(new Intent(TitlePage.this, AboutPage.class));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_title_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
